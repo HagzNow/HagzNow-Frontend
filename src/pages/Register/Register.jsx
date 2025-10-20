8ca1694c5e50568b0c2e7e5245f160cc830c6c49
mohamedelserougy <mohamedelserougy22@gmail.com>
2025-10-20 12:10:12 +0300

    feat: add Register component

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <>
+      <section className="container ">
+        <div className="title bg-amber-900 ju">
+          <h2 className=" text-4xl font-bold text-mainColor ">ارينا بوك</h2>
+          <h2 className="text-4xl font-bold">إنشاء حساب في أرينا بوك</h2>
+          <p className=" font-medium text-secondColor">
+            سجل لتبدأ رحلتك الرياضية
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
