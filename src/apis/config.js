463a281f73a054141ec5622e30404a8defa5f3c1
mohamedelserougy <mohamedelserougy22@gmail.com>
2025-11-02 21:11:02 +0200

    feat: add wallet top-up request and redirect user to Paymob payment page

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+
 const BASE_URL = import.meta.env.VITE_API_URL;
+
 const baseUrl = axios.create({
   baseURL: BASE_URL,
 });
+
+baseUrl.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default baseUrl;
