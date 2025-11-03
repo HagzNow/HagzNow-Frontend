09d91d62254109d82fdecd4815f7d620ee8d3feb
Mahamedmamdouh <mada.8054@gmail.com>
2025-11-03 21:48:35 +0200

    feat: improve the design

--- src/components/AdminLayout/AdminFooter.jsx
+++ src/components/AdminLayout/AdminFooter.jsx
@@ -1,7 +1,7 @@
 export default function AdminFooter() {
   return (
     <footer className="border-t border-neutral-200 dark:border-neutral-800 bg-white dark:bg-neutral-900">
-      <div className="mx-auto max-w-7xl px-4 py-6 text-sm text-neutral-500 flex items-center justify-between">
+      <div className="mx-auto max-w-7xl px-4 py-6 text-sm text-neutral-500 flex items-center justify-between ">
         <div className="flex items-center gap-4">
           <span>الشروط</span>
           <span>الخصوصية</span>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,25 +1,3 @@
-// import React, { useContext } from "react";
-// import { Outlet } from "react-router-dom";
-// import Navbar from "../Navbar";
-// import Footer from "../Footer";
-// import { authContext } from "../../Contexts/AuthContext";
-// import AdminNavbar from "../AdminLayout/AdminNavbar";
-// import AdminFooter from "../AdminLayout/AdminFooter";
-
-// export default function Layout() {
-//   let { user } = useContext(authContext);
-
-//   return (
-//     <div>
-//       {user?.role === "admin" ? <AdminNavbar /> : <Navbar />}
-//       <div >
-//         <Outlet />
-//       </div>
-//       {user?.role === "admin" ? <AdminFooter /> : <Footer />}
-//     </div>
-//   );
-// }
-
 import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
@@ -28,27 +6,38 @@ import { authContext } from "../../Contexts/AuthContext";
 import AdminNavbar from "../AdminLayout/AdminNavbar";
 import AdminFooter from "../AdminLayout/AdminFooter";
 import Header from "../OwnerComponents/Header";
-// import Header from ;
 
 export default function Layout() {
   const { user } = useContext(authContext);
-  let HeaderCustom = Navbar;
+
+  let HeaderComponent = Navbar;
   let FooterComponent = Footer;
+
   if (user?.role === "admin") {
-    HeaderCustom = AdminNavbar;
+    HeaderComponent = AdminNavbar;
     FooterComponent = AdminFooter;
   } else if (user?.role === "owner") {
-    HeaderCustom = Header;
-    // FooterComponent = AdminFooter;
+    HeaderComponent = Header;
+    FooterComponent = AdminFooter;
   }
 
   return (
     <div className="app-layout">
-      <HeaderCustom />
+      {user?.role !== "owner" && ""} {/* ✅ كده الأخضر مش هيظهر */}
       <main className="content">
         <Outlet />
       </main>
-      <FooterComponent />
-    </div>
+      <div
+  className={` z-50 relative ${
+    user?.role === "owner" ? "mr-[270px]" : ""
+  }`}
+>
+  {FooterComponent && <AdminFooter />}
+</div>
+</div>
+ 
+ 
   );
 }
+
+
